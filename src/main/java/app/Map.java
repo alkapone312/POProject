665c647cf87fa937c7d3f82807c4abe45e985cfb
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2022-05-19 15:11:06 +0200

    Basic project template

--- src/main/java/app/Map.java
+++ src/main/java/app/Map.java
@@ -2,6 +2,7 @@ package app;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 
 //MAP GENERATIONS IS BASED ON 2D PERLIN NOISE GENERATOR
@@ -21,11 +22,12 @@ public class Map {
     {
         Color color;
         if(!mapBuffered) {
+            Random r = new Random();
             for (int x = 0; x < Reference.COLS; x++) {
                 for (int y = 0; y < Reference.ROWS; y++) {
                     switch (cMap[x][y]){
                         default:
-                            color = Color.GRAY;
+                            color = new Color((int)(r.nextDouble()*255));
                             mapBufferGraphics.setColor(color);
                             mapBufferGraphics.fillRect(x*Reference.PIXEL_SIZE,y*Reference.PIXEL_SIZE, Reference.PIXEL_SIZE, Reference.PIXEL_SIZE);
                             break;
