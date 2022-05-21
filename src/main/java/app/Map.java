5a0c1d0142319f2d8e48766540a016034a454fd9
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2022-05-21 19:10:49 +0200

    Readme & todo in Map.java

--- src/main/java/app/Map.java
+++ src/main/java/app/Map.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 //MAP GENERATIONS IS BASED ON 2D PERLIN NOISE GENERATOR
 public class Map {
+    // TODO: Stworzenie mapy ze znaków ascii
     private char[][] cMap = new char[Reference.COLS][Reference.ROWS];
     private BufferedImage mapBuffer;
     private Graphics2D mapBufferGraphics;
@@ -25,14 +26,16 @@ public class Map {
             Random r = new Random();
             for (int x = 0; x < Reference.COLS; x++) {
                 for (int y = 0; y < Reference.ROWS; y++) {
+                    // TODO: Wykorzystać tego switcha do narysowania mapy
+                    // On przelatuje przez wszystkie znaki jakie są w zmiennej cMap i może ją przerobić na konkretne kolory, dla każdego case'a po prostu trzeba dać inny kolor
                     switch (cMap[x][y]){
                         default:
                             color = new Color((int)(r.nextDouble()*255),(int)(r.nextDouble()*245),(int)(r.nextDouble()*215));
-                            mapBufferGraphics.setColor(color);
-                            mapBufferGraphics.fillRect(x*Reference.PIXEL_SIZE,y*Reference.PIXEL_SIZE, Reference.PIXEL_SIZE, Reference.PIXEL_SIZE);
                             break;
-
                     }
+                    //tutaj kolor jest drukowany do mapki w odpowiednim x i y
+                    mapBufferGraphics.setColor(color);
+                    mapBufferGraphics.fillRect(x*Reference.PIXEL_SIZE,y*Reference.PIXEL_SIZE, Reference.PIXEL_SIZE, Reference.PIXEL_SIZE);
                 }
             }
             this.mapBuffered = true;
