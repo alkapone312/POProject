d813a7d584a359df6bca4c063cfc1d93db64e16f
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2022-06-14 00:04:29 +0200

    Add worker reset at day start

--- src/main/java/app/Utils/HR.java
+++ src/main/java/app/Utils/HR.java
@@ -3,10 +3,12 @@ package app.Utils;
 import app.ControlPoint;
 import app.Factory;
 import app.Machine.Machine;
+import app.Reference;
 import app.Settings;
 import app.Worker.Worker;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class HR {
     public static void setWorkstand(Worker w, Machine m) {
@@ -16,6 +18,12 @@ public class HR {
 
     public static void workerRoutine(Worker worker) {
         if(worker.path.size() == 0 || Factory.dayTime == 0) {
+            if (Factory.dayTime == 0) {
+                Random r = new Random();
+                worker.setX(r.nextInt(14) + 6);
+                worker.setY(r.nextInt(4) + Reference.ROWS - 3);
+                worker.path = new ArrayList<>();
+            }
             if (!worker.isTired()){
                 if (!worker.isWorking() && Factory.dayTime < 3000) {
                     worker.path = (ArrayList<ControlPoint>) Factory.entranceWorkPath.clone();
